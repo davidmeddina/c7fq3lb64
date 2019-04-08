888af2cef6c974af3efe98228c38e22dc62017e1
david medina <davidmeddina@gmail.com>
2019-04-08 17:59:44 -0500

    Filtering feature corrected

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,13 +1,11 @@
 class ExpensesController < ApplicationController
-  def index
+ def index
     @expenses = Expense.order("date DESC")
     if params[:concept].present?
-      @expenses = @expenses.where("concept = ?", params[:concept])
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
     end
     if params[:category_id].present?
       @expenses = @expenses.where("category_id = ?", params[:category_id])
-    end
+    end  
   end
-  
-
 end
